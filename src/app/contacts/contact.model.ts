ddc8bf52ffdb9fa2d1b8b5292b79e949eaccca93
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2023-01-13 14:32:39 +0200

    cms

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,8 @@
+export class Contact {
+   public contct_id: number;
+   public name: string;
+   public email: string;
+   public phone_Number: number;
+   public imageUrl:  string;
+   public group: string;
+}
