4958185681fb898e4e67a88b16eb65df4a249276
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2023-01-29 23:42:57 +0200

    fixing document template complited

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -11,11 +11,11 @@ export class DocumentListComponent {
   @Output()  selectedDocumentEven: EventEmitter<Document> = new EventEmitter<Document>();
   // selectedContactEven:  = new EventEmitter<Contact>();
   documents: Document[] = [
-    new Document(1,'example1','example1', 'example1','null'),
-    new Document(1,'example2','example2', 'example2','null'),
-    new Document(1,'example3','example3', 'example3','null'),
-    new Document(1,'example4','example4', 'example4','null'),
-    new Document(1,'example5','example5', 'example6','null'),
+    new Document(1,'Maths','example1', 'example1','null'),
+    new Document(2,'Shona','example2', 'example2','null'),
+    new Document(3,'History','example3', 'example3','null'),
+    new Document(4,'English','example4', 'example4','null'),
+    new Document(5,'Economics','example5', 'example6','null'),
 
 
 ];
